a8e755e642d301dca77a05fb0eb86c31b7a96a54
tianopo <matheuslink18@hotmail.com>
2024-01-27 22:46:03 -0300

    efeito da Logo pronto

--- app/components/Layout/Header/Logo.tsx
+++ app/components/Layout/Header/Logo.tsx
@@ -1,11 +1,38 @@
-import { StyledLogo, StyledImage, StyledWrite } from "../../../../styles/componentLayout";
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { StyledImage, StyledLogo, StyledWrite } from "../../../../styles/componentLayout";
 
 export const Logo = (): any => {
+  const router = useRouter();
+  const [isHomePage, setIsHomePage] = useState(router.pathname === '/');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsHomePage(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <StyledLogo>
+    <StyledLogo
+      style={{
+        width: !isHomePage ? "62px" : "259px",
+        justifyContent: !isHomePage ? "center" : "space-around",
+        transition: "width 1s ease"
+      }}
+      onMouseEnter={() => setIsHomePage(true)}
+      onMouseLeave={() => setIsHomePage(false)}
+    >
       <StyledImage src="/images/white-pokeball.svg" alt='pokebola' />
-      <StyledWrite>Centro Pokémon</StyledWrite>
+      <StyledWrite
+        style={{
+          opacity: !isHomePage ? 0 : 1,
+          transition: "opacity 2s ease"
+        }}
+      >
+        {!isHomePage ? "" : "Centro Pokémon"}
+      </StyledWrite>
     </StyledLogo>
   );
-}
+};
